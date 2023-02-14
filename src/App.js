d6be32b997b0d3cb4f68d1d3d29a069f736f2af8
Gregory Lienard <lienardgregory16@gmail.com>
2023-02-14 16:33:47 +0100

    ajout logements et caroussel, modifications app.js et modif' mineure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-/*import Header from './components/header/header'
-import Navbar from './components/navbar/navbar'*/
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./page/About/About";
 import Home from "./page/Home/Home";
+import Logement from "./page/Logement/Logement";
+import Error from "./page/Error/Error";
+
+
 
 export default function App() {
     return (
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/Logement/:id" element={<Logement />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     );
